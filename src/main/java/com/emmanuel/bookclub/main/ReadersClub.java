cb51ec415daaa93bb87cde0003a59a4a6a3da521
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2017-10-10 15:32:01 +0100

    implement requesting, lending and returning of books

--- src/main/java/com/emmanuel/bookclub/main/ReadersClub.java
+++ src/main/java/com/emmanuel/bookclub/main/ReadersClub.java
@@ -3,17 +3,15 @@ package com.emmanuel.bookclub.main;
 import com.emmanuel.bookclub.models.Book;
 import com.emmanuel.bookclub.models.Member;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ReadersClub {
     List<Book> readersClubBooks;
     Map<Member, List<Book>> readersClubMembers;
-//    Map<String, List<Book>> readersClubBooks;
+    BookRequestQueue bookRequestQueue;
 
-    public ReadersClub() {
+    public ReadersClub(BookRequestQueue bookRequestQueue) {
+        this.bookRequestQueue = bookRequestQueue;
         readersClubBooks = new ArrayList<Book>();
         readersClubMembers = new HashMap<Member, List<Book>>();
     }
@@ -60,6 +58,48 @@ public class ReadersClub {
         if(!checkMemberExistInClub(member)){
             ArrayList memberBooksBorrowed = new ArrayList();
             readersClubMembers.put(member, memberBooksBorrowed);
+        }
+    }
+
+    public void requestForBook(Book bookRequestedFor, Member memberRequesting) {
+        if(checkBookExistInClub(bookRequestedFor) && checkMemberExistInClub(memberRequesting)) {
+            bookRequestQueue.addMemberToQueue(bookRequestedFor, memberRequesting);
+        }
+    }
+
+    protected boolean checkBookAvailableForLending(Book bookRequested){
+        return checkBookExistInClub(bookRequested) && ((bookRequested.getNumberOfCopies() - bookRequested.getBorrowCount()) > 0);
+    }
+
+    public Member giveBookToEligibleMember(Book bookRequested){
+        if(checkBookAvailableForLending(bookRequested)){
+            Member memberWhoGetsBook = bookRequestQueue.getEligibleMemberToBorrowBook(bookRequested);
+            bookRequested.incrementBorrowCount(1);
+            updateListOfMemberBorrowedBooks(memberWhoGetsBook, bookRequested);
+            return memberWhoGetsBook;
+        }
+        return null;
+    }
+
+    private void updateListOfMemberBorrowedBooks (Member memberWhoGetsBook, Book bookRequested ) {
+        if(checkMemberExistInClub(memberWhoGetsBook)){
+            List<Book> booksBorrowedByMember = readersClubMembers.get(memberWhoGetsBook);
+            booksBorrowedByMember.add(bookRequested);
+            readersClubMembers.put(memberWhoGetsBook, booksBorrowedByMember);
+        } else {
+            List<Book> booksBorrowedByMember = new ArrayList<Book>();
+            booksBorrowedByMember.add(bookRequested);
+            readersClubMembers.put(memberWhoGetsBook, booksBorrowedByMember);
+        }
+    }
+
+    public void returnBookToClub(Member memberReturningBook, Book bookMemberIsReturning){
+        List<Book> booksBorrowedByMember = readersClubMembers.get(memberReturningBook);
+
+        if(booksBorrowedByMember.contains(bookMemberIsReturning)){
+            bookMemberIsReturning.decrementBorrowCount(1);
+            booksBorrowedByMember.remove(bookMemberIsReturning);
+            readersClubMembers.put(memberReturningBook, booksBorrowedByMember);
         }
     }
 }
--- src/test/java/com/emmanuel/bookclub/main/ReadersClubTest.java
+++ src/test/java/com/emmanuel/bookclub/main/ReadersClubTest.java
@@ -21,13 +21,13 @@ public class ReadersClubTest {
     private Book book2 = new Book("General Mathematics", "G.U Garba", "346tyu", 1);
     private Book book3 = new Book("Jero Play", "Wole Soyinka", "34ert", 6);
 
-//    List<Book> bookList = new ArrayList<Book>(Arrays.asList(book1, book2));
-    ReadersClub readersClub = new ReadersClub();
+    private BookRequestQueue bookRequestQueue = new BookRequestQueue();
+    //    List<Book> bookList = new ArrayList<Book>(Arrays.asList(book1, book2));
+    private ReadersClub readersClub = new ReadersClub(bookRequestQueue);
 
     @Test
-    public void testAddBooksToClub() {
+    public void testAddBooksToClub() throws Exception {
         readersClub.addBooksToClub(new Book[] { book1, book2 });
-
         assertTrue(book1.getNumberOfCopies() == 3);
         assertTrue(book2.getNumberOfCopies() == 1);
         assertTrue(readersClub.readersClubBooks.size() == 2);
@@ -41,7 +41,7 @@ public class ReadersClubTest {
     }
 
     @Test
-    public void testAddBookToClub() {
+    public void testAddBookToClub() throws Exception {
         readersClub.addBookToClub(book1);
         assertTrue(book1.getNumberOfCopies() == 3);
         assertTrue(readersClub.readersClubBooks.size() == 1);
@@ -55,19 +55,21 @@ public class ReadersClubTest {
     }
 
     @Test
-    public void testAddMembersToClub() {
+    public void testAddMembersToClub() throws Exception {
         Member[] memberArray = {student1, staff1};
-        readersClub.addMembersToClub(memberArray);
 
+        readersClub.addMembersToClub(memberArray);
         assertEquals(readersClub.readersClubMembers.size(),2);
     }
 
     @Test
-    public void testAddMemberToClub() {
+    public void testAddMemberToClub() throws Exception {
         readersClub.addMemberToClub(staff2);
         assertEquals(readersClub.readersClubMembers.size(),1);
+
         readersClub.addMemberToClub(staff2);
         assertEquals(readersClub.readersClubMembers.size(),1);
+
         readersClub.addMemberToClub(student2);
         assertEquals(readersClub.readersClubMembers.size(),2);
     }
@@ -76,6 +78,7 @@ public class ReadersClubTest {
     public void testCheckMemberExistInClub() throws Exception {
         assertFalse(readersClub.checkMemberExistInClub(staff1));
         assertFalse(readersClub.checkMemberExistInClub(student1));
+
         readersClub.addMembersToClub(new Member[] {staff1, student1});
         assertTrue(readersClub.checkMemberExistInClub(staff1));
         assertTrue(readersClub.checkMemberExistInClub(student1));
@@ -85,8 +88,76 @@ public class ReadersClubTest {
     public void testCheckBookExistInClub() throws Exception {
         assertFalse(readersClub.checkBookExistInClub(book3));
         assertFalse(readersClub.checkBookExistInClub(book2));
+
         readersClub.addBooksToClub(new Book[] {book3, book2});
         assertTrue(readersClub.checkBookExistInClub(book3));
         assertTrue(readersClub.checkBookExistInClub(book2));
+    }
+
+    @Test
+    public void testRequestForBook() throws Exception {
+        readersClub.addBooksToClub(new Book[] {book3, book2});
+        readersClub.addMembersToClub(new Member[] {staff1, student1});
+        readersClub.requestForBook(book3,staff1);
+        readersClub.requestForBook(book3,student1);
+        readersClub.requestForBook(book2,staff1);
+
+        assertTrue(bookRequestQueue.checkQueueForMember(book3,staff1));
+        assertTrue(bookRequestQueue.checkQueueForMember(book3,student1));
+        assertFalse(bookRequestQueue.checkQueueForMember(book2,staff2));
+        assertFalse(bookRequestQueue.checkQueueForMember(book2,student1));
+    }
+
+    @Test
+    public void testCheckBookAvailableForLending() {
+        book1.setBorrowCount(1);
+        book3.setBorrowCount(2);
+        readersClub.addBooksToClub(new Book[] {book2, book3});
+
+        assertTrue(readersClub.checkBookAvailableForLending(book3));
+        assertFalse(readersClub.checkBookAvailableForLending(book1));
+    }
+
+    @Test
+    public void testGiveBookToEligibleMember() {
+        readersClub.addBooksToClub(new Book[] {book1, book2});
+        readersClub.addMembersToClub(new Member[] {staff1, student1, student2});
+        readersClub.requestForBook(book1,staff1);
+        readersClub.requestForBook(book1,student1);
+        readersClub.requestForBook(book2,student1);
+        readersClub.requestForBook(book2,student2);
+
+        Member memberGivenBook = readersClub.giveBookToEligibleMember(book1);
+        Member memberGivenBook2 = readersClub.giveBookToEligibleMember(book2);
+
+        assertFalse(memberGivenBook.equals(student1));
+        assertTrue(memberGivenBook.equals(staff1));
+        assertTrue(readersClub.giveBookToEligibleMember(book1).equals(student1));
+        assertFalse(memberGivenBook2.equals(student2));
+        assertTrue(memberGivenBook2.equals(student1));
+
+        // checks for situation book not available to be given to member if number of copies available in club library less than 1
+        // takes into consideration if boot borrowed count equals to book copy
+        assertEquals(null, readersClub.giveBookToEligibleMember(book2));
+        assertEquals(1, book2.getBorrowCount());
+    }
+
+    @Test
+    public void testReturnBookToClub() {
+        readersClub.addBooksToClub(new Book[] {book1});
+        readersClub.addMembersToClub(new Member[] {staff2, student1});
+        readersClub.requestForBook(book1,staff2);
+        readersClub.requestForBook(book1,student1);
+
+        assertEquals(0,book1.getBorrowCount());
+        readersClub.giveBookToEligibleMember(book1);
+        assertEquals(1,book1.getBorrowCount());
+        readersClub.giveBookToEligibleMember(book1);
+        assertEquals(2,book1.getBorrowCount());
+
+        readersClub.returnBookToClub(staff2,book1);
+        assertEquals(1, book1.getBorrowCount());
+        readersClub.returnBookToClub(student1,book1);
+        assertTrue(book1.getBorrowCount() == 0);
     }
 }
